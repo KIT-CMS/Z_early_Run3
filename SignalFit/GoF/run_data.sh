fa6acd803d054b39a85ab5f9a4ae3d2ce3e15448
Nils Faltermann <nils.faltermann@cern.ch>
2023-02-24 14:53:32 +0100

    gof

--- SignalFit/GoF/run_data.sh
+++ SignalFit/GoF/run_data.sh
@@ -8,5 +8,5 @@ myname="_${gof_algo}"
 #model_specific_options="--maskedChan muplus_xsec --maskedChan muminus_xsec --maskedChan mumu_xsec --X-allow-no-background"
 model_specific_options=""
 
-combine -M GoodnessOfFit -n $myname -m 125 ${datacard_name_noxs}.txt ${model_specific_options} --algo=${gof_algo} ${gof_toys}
+combine -M GoodnessOfFit -n $myname -m 125 -d ${gof_input} ${model_specific_options} --algo=${gof_algo} ${gof_toys}
 mv higgsCombine${myname}.GoodnessOfFit.mH125.root ${gof_toy_dir}/
