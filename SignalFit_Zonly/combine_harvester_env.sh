988f2f9e7d8bc4ce8a138e122917564e4ea58a32
jdriesch <jost.driesch@kit.edu>
2023-10-13 10:14:36 +0200

    z only not needed anymore

--- SignalFit_Zonly/combine_harvester_env.sh
+++
@@ -1,7 +0,0 @@
-
-
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc7_amd64_gcc900
-cd /work/nfaltermann/cmssw/CMSSW_11_3_4/src/
-cmsenv
-cd -
--- SignalFit_Zonly/modules/CombineHarvester/README.md
+++
@@ -1,2 +0,0 @@
-some scripts taken directly from [CombineHavester](https://cms-analysis.github.io/CombineHarvester/).
-Mostly for plotting impacts.
